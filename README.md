32da1b979e85a27647fe221f93e67a5f1e2cf551
c-ferraz <cs.ferraz@outlook.com>
2020-12-02 23:53:54 -0300

    Revert "primeiro commit no repo"
    This reverts commit 743b72dc8276262dd681af7542a13991b9a01437.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Como executar no IDE
+Importar ou dar pull no projeto, o Maven resolve as dependencias.  
+Executar a classe '/src/test/java/com/conexaoporto/springboot/ConexaoPortoApplicationTests.java' como 'Aplicação Java' para iniciar a aplicação.  
+
+# Como construir o projeto no maven e executar
+Em breve  
+
+# Como executar apenas o .jar
+Em breve  
+
+# Prévia do site
+O site mais atual pode ser acessado pelo link: https://conexao-porto.herokuapp.com/  
+Existe uma 'Área de testes' no endereço https://conexao-porto.herokuapp.com/test com algumas coisas ainda não implementadas.
---
+++ src/main/java/com/conexaoporto/springboot/controllers/ProfissionalController.java
@@ -0,0 +1,63 @@
+package com.conexaoporto.springboot.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.conexaoporto.springboot.model.entities.Profissional;
+import com.conexaoporto.springboot.model.repositories.ProfissionalRepository;
+
+@Controller
+@RequestMapping
+public class ProfissionalController {
+	
+	@Autowired
+	ProfissionalRepository profissionalRepo;
+	
+	@PostMapping("/CadastrarProfissional")
+	public String cadastro (@RequestParam(name= "nome") String nome,
+			@RequestParam(name="senha") String senha,
+			@RequestParam(name="email") @Valid String email,
+			Model model)
+			 {
+		if (profissionalRepo.findByEmail(email) != null) {
+			model.addAttribute("emailError", "O email informado já esta cadastrado.");
+			return "cadastro";
+		}
+		
+		profissionalRepo.save(new Profissional(nome, email, senha));
+		return "redirect:/login";
+	}
+	
+	@PostMapping("loginUsuario")
+	public String login(@RequestParam(name= "email") String email,
+			@RequestParam(name= "senha") String senha,
+			Model model,
+			HttpSession session) {
+		if (email.isEmpty() || senha.isEmpty()) {
+			model.addAttribute("erroDeAutenticacao", "E-mail ou senha inválidos.");
+			return "login";
+		}
+		
+		Profissional usuario = profissionalRepo.findByEmail(email) != null ? profissionalRepo.findByEmail(email) : null;
+		if ((usuario != null) && usuario.getSenha().contentEquals(senha)) {
+			session.setAttribute("userId", usuario.getCodUsuario());
+			session.setAttribute("userEmail", usuario.getEmail());
+			session.setMaxInactiveInterval(300); // TODO: alterar para um numero maior quando terminar de testar
+			return "redirect:/home";
+		} else {
+			model.addAttribute("erroDeAutenticacao", (usuario == null) ? "Esse e-mail não está cadastrado no sistema." : "Senha incorreta.");
+			return "login";
+		}
+	}
+	
+
+	
+}
